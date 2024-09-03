898463bcd418d2643258d3cfb4caa82bccbca450
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-09-03 19:53:18 +0800

    Minor change: width of form input

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,7 +3,7 @@ import Input from "./Input";
 
 export default function NewProject() {
   return (
-    <div className="w-[90%] md:w-1/2 mx-auto md:mx-0 my-6">
+    <div className="w-[90%] md:w-1/2 2xl:w-1/3 mx-auto md:mx-0 my-6">
       <menu className="flex justify-end gap-2">
         <li>
           <Button>Cancel</Button>
@@ -13,7 +13,7 @@ export default function NewProject() {
         </li>
       </menu>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 ">
         <Input label="title" />
         <Input label="Description" textarea />
         <Input label="Due Date" />
