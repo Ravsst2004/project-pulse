3a21897f4e65ba14433c6dd7db4eba0724266b3f
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-09-04 21:31:44 +0800

    Change font size

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -3,10 +3,10 @@ import Button from "./Button";
 export default function NoProjectSelected({ handleStartProject }) {
   return (
     <div className="w-[90%] md:w-1/2 mx-auto my-20 text-center">
-      <h1 className="text-4xl md:text-6xl text-stone-700">
+      <h1 className="text-2xl md:text-6xl text-stone-700">
         No Project Selected
       </h1>
-      <p className="text-xl md:text-3xl my-2">
+      <p className="text-lg md:text-3xl my-2">
         Select a project or create a new one
       </p>
       <Button onClick={handleStartProject} className={"md:text-2xl"}>
